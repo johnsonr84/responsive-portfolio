1a1d7bceef672a404a75994c98cf891d30adbe05
Robert Johnson <robertcjohnson1984@gmail.com>
2021-02-10 11:43:57 -0700

    add style to homepage and intro, numbers and dev sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 // Create a request variable and assign a new XMLHttpRequest object to it 
-var request = new HMLHttpResquest()
+var request = new XMLHttpRequest()
 
 //Open a new connection, using the GET request on the URL
-request.open('GET', 'https://api.github.com/users/johnsonr84/repos', true)
+request.open('GET', 'https://docs.github.com/repos/johnsonr84/stats/commit_activity', true)
 
 request.onload = function () {
     //Begin accessing JSON data here
     var data = JSON.parse(this.response);
+    console.log(data)
+
+    var statusHTML = '';
+    $.each(data, function(i, status) {
+        statusHTML = '<span>' + status.days + '</span>';
+
+    });
 }
 
 // Send request
