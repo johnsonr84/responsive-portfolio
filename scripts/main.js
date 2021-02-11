1f2201f11c3e6852fdd662da4a0ef92b48f84a21
Robert Johnson <robertcjohnson1984@gmail.com>
2021-02-11 14:37:55 -0700

    add js to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,13 @@
-// var request = new XMLHttpRequest()
-
-// //Open a new connection, using the GET request on the URL
-// request.open('GET', 'https://docs.github.com/repos/johnsonr84/stats/commit_activity', true)
-
-// request.onload = function () {
-//     //Begin accessing JSON data here
-//     var data = JSON.parse(this.response);
-//     console.log(data)
-
-//     // var statusHTML = '';
-//     // $.each(data, function(i, status) {
-//     //     statusHTML = '<span>' + status.days + '</span>';
-
-//     // });
-// }
-
-// // Send request
-// request.send();
+{
+    user(login: "johnsonr84") {
+      repositories {
+        totalCount
+      }
+      name
+      contributionsCollection {
+        contributionCalendar {
+          totalContributions
+        }
+      }
+    }
+  }
